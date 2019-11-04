6617ba467a568c0a81b5beeab2d694d40b6668c0
mydeskmate <516622145@qq.com>
2019-11-04 13:56:00 +0800

    deploy

--- django_deploy/django_deploy/settings.py
+++ django_deploy/django_deploy/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
-# STATIC_ROOT = os.path.join(BASE_DIR,'uuuuuu')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
+
+# uwsgi 静态文件路径
+STATIC_ROOT = os.path.join(BASE_DIR,'uuuuuu')
